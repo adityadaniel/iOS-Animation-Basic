b011a680045a408cf977df9cd62a706b7e552cc0
Daniel Aditya Istyana <danieladityaistyana@gmail.com>
2019-05-16 21:18:02 +0700

    Initial Commit

---
+++ iOS Animation/ViewController.swift
@@ -0,0 +1,90 @@
+//
+//  ViewController.swift
+//  iOS Animation
+//
+//  Created by Daniel Aditya Istyana on 16/05/19.
+//  Copyright © 2019 Daniel Aditya Istyana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var randomHeight: () -> CGFloat = { () in
+        return CGFloat.random(in: 0...UIScreen.main.bounds.height)
+    }
+    
+    var randomWidth: () -> CGFloat = { () in
+        return CGFloat.random(in: 0...UIScreen.main.bounds.width)
+    }
+    
+    
+    @IBOutlet weak var animationTypeSegmentedControl: UISegmentedControl! {
+        didSet {
+            animationTypeSegmentedControl.tintColor = .red
+        }
+    }
+    @IBOutlet weak var redView: UIView!
+    
+    @IBAction func animationTypeSegmentedControlTapped(_ sender: UISegmentedControl) {
+        print(animationTypeSegmentedControl.selectedSegmentIndex)
+    }
+    
+    override func viewDidLoad() {
+        
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(startAnimation))
+        gesture.numberOfTapsRequired = 1
+        
+        redView.isUserInteractionEnabled = true
+        redView.addGestureRecognizer(gesture)
+        
+        super.viewDidLoad()
+        
+    }
+    
+    @objc func startAnimation() {
+        if animationTypeSegmentedControl.selectedSegmentIndex == 0 {
+            redView.layer.removeAllAnimations()
+            animateBounce()
+        } else if animationTypeSegmentedControl.selectedSegmentIndex == 1 {
+            redView.layer.removeAllAnimations()
+            animateSlide()
+        } else {
+            redView.layer.removeAllAnimations()
+            animatePop()
+        }
+    }
+    
+    func animatePop() {
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 0.5, options: .transitionCurlUp, animations: {
+            let frame = self.redView.frame
+            self.redView.frame = CGRect(x: frame.origin.x, y: frame.origin.y, width: 0, height: 0)
+        }) { _ in
+            UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 0.5, options: [.curveEaseInOut], animations: {
+                self.redView.frame = CGRect(x: self.randomWidth(), y: self.randomHeight(), width: 200, height: 200)
+                self.redView.layer.cornerRadius = self.redView.frame.height / 2
+                self.redView.backgroundColor = self.createRandomColor()
+            }, completion: nil)
+        }
+    }
+    
+    func animateBounce() {
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [.curveLinear], animations: {
+            self.redView.frame = CGRect(x: self.randomWidth(), y: self.randomHeight(), width: 200, height: 200)
+            print(self.randomHeight, self.randomWidth)
+            self.redView.backgroundColor = self.createRandomColor()
+        }, completion: nil)
+    }
+    
+    func animateSlide() {
+        UIView.animate(withDuration: 2, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveLinear, animations: {
+            self.redView.frame = CGRect(x: self.randomWidth(), y: self.randomHeight(), width: 200, height: 200)
+            self.redView.backgroundColor = self.createRandomColor()
+            
+        }, completion: nil)
+    }
+    
+    func createRandomColor() -> UIColor {
+        return UIColor(red: CGFloat.random(in: 0...255)/255, green: CGFloat.random(in: 0...255)/255, blue: CGFloat.random(in: 0...255)/255, alpha: 1.0)
+    }
+}

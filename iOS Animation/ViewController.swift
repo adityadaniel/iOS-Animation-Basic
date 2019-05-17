8c326e2f0965149a6e7b326718271fff06ca4709
Daniel Aditya Istyana <danieladityaistyana@gmail.com>
2019-05-17 09:58:10 +0700

    Change randomHeight and randomWeight upperbound. \

--- iOS Animation/ViewController.swift
+++ iOS Animation/ViewController.swift
@@ -11,20 +11,23 @@ import UIKit
 class ViewController: UIViewController {
     
     var randomHeight: () -> CGFloat = { () in
-        return CGFloat.random(in: 0...UIScreen.main.bounds.height)
+        return CGFloat.random(in: 0...700)
     }
     
     var randomWidth: () -> CGFloat = { () in
-        return CGFloat.random(in: 0...UIScreen.main.bounds.width)
+        return CGFloat.random(in: 0...300)
     }
+    
+    @IBOutlet weak var redView: UIView!
     
     
     @IBOutlet weak var animationTypeSegmentedControl: UISegmentedControl! {
         didSet {
             animationTypeSegmentedControl.tintColor = .red
+            animationTypeSegmentedControl.layer.cornerRadius = 12
         }
     }
-    @IBOutlet weak var redView: UIView!
+    
     
     @IBAction func animationTypeSegmentedControlTapped(_ sender: UISegmentedControl) {
         print(animationTypeSegmentedControl.selectedSegmentIndex)
@@ -34,6 +37,8 @@ class ViewController: UIViewController {
         
         let gesture = UITapGestureRecognizer(target: self, action: #selector(startAnimation))
         gesture.numberOfTapsRequired = 1
+        
+        redView.bringSubviewToFront(animationTypeSegmentedControl)
         
         redView.isUserInteractionEnabled = true
         redView.addGestureRecognizer(gesture)
@@ -56,13 +61,13 @@ class ViewController: UIViewController {
     }
     
     func animatePop() {
-        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 0.5, options: .transitionCurlUp, animations: {
+        redView.layer.cornerRadius = redView.bounds.height / 2
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
             let frame = self.redView.frame
             self.redView.frame = CGRect(x: frame.origin.x, y: frame.origin.y, width: 0, height: 0)
         }) { _ in
             UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 0.5, options: [.curveEaseInOut], animations: {
                 self.redView.frame = CGRect(x: self.randomWidth(), y: self.randomHeight(), width: 200, height: 200)
-                self.redView.layer.cornerRadius = self.redView.frame.height / 2
                 self.redView.backgroundColor = self.createRandomColor()
             }, completion: nil)
         }
